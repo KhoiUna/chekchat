e3997690fd4ed9c3dc6e23eca9602a0367582734
KhoiNguyen <thorwaitson@gmail.com>
2021-03-17 19:33:58 -0500

    update bell badge

--- client/components/bell.js
+++ client/components/bell.js
@@ -1,23 +1,28 @@
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import Link from "next/link";
 import utilStyles from "../styles/utils.module.css";
-import NotificationBadge from "react-notification-badge";
-import { Effect } from "react-notification-badge";
+import Badge from "@material-ui/core/Badge";
 
 export default function Bell({ componentName, notificationCount }) {
   return (
     componentName !== "Notifications" && (
       <Link href="/notifications">
-        <div style={{ display: "flex" }}>
-          <NotificationsIcon
-            className={utilStyles.icon}
-            style={{ fontSize: "2.5rem" }}
-          />
-          <NotificationBadge
-            count={notificationCount || 10}
-            effect={Effect.SCALE}
-            style={{ top: "1.2rem" }}
-          />
+        <div>
+          <Badge
+            badgeContent={notificationCount || 100}
+            color="error"
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "right",
+            }}
+            max={99}
+            overlap="circle"
+          >
+            <NotificationsIcon
+              className={utilStyles.icon}
+              style={{ fontSize: "2rem" }}
+            />
+          </Badge>
         </div>
       </Link>
     )
