26eb4aebc46c5acefcd4d8ae77f563025ff41349
KhoiNguyen <thorwaitson@gmail.com>
2021-03-18 02:05:44 -0500

    update index.js

--- client/pages/index.js
+++ client/pages/index.js
@@ -5,6 +5,13 @@ export default function Home() {
   return (
     <div>
       <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="description" content="The app for assigning missions" />
+        <link rel="image_src" href="/logo192.png" />
+        <link rel="apple-touch-icon" href="/logo192.png" />
+        <link rel="manifest" href="/manifest.json" />
         <title>ChekThat | Home</title>
       </Head>
 
