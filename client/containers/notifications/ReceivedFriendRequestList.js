dc98b2b1bb06914b97ba9968fce3474ab614b4de
KhoiNguyen <thorwaitson@gmail.com>
2021-03-21 01:40:09 -0500

    update friend request notifications

--- client/containers/notifications/ReceivedFriendRequestList.js
+++ client/containers/notifications/ReceivedFriendRequestList.js
@@ -1,22 +1,39 @@
 import { useState, useEffect } from "react";
 import FriendRequest from "../../components/friends/friend_request";
-import { fetchReceivedFriendRequestsList } from "../../utils/FriendRequest";
+import {
+  fetchReceivedFriendRequestsList,
+  actionFriendRequest,
+} from "../../utils/FriendRequest";
+import removeItemFromFriendRequest from "../../helpers/removeItemFromFriendRequest";
 
 export default function ReceivedFriendRequestList() {
   const [friendRequestList, setFriendRequestList] = useState([]);
   useEffect(() => {
+    console.log("fetch");
     fetchReceivedFriendRequestsList().then((r) => setFriendRequestList(r));
   }, []);
 
+  const [change, setChange] = useState(false);
+  const handleClick = async (action, requestId) => {
+    const res = await actionFriendRequest(action, requestId);
+    setChange(!change);
+    setFriendRequestList(
+      (prev) => (prev = removeItemFromFriendRequest(prev, requestId))
+    );
+  };
+
   return (
     <>
-      {friendRequestList.map((item) => (
+      {friendRequestList.map((item, index) => (
         <FriendRequest
+          key={index}
           fromPage="notifications"
+          requestId={item._id}
           username={item.from.username}
           email={item.from.email}
           avatarURL={item.from.avatarURL}
           status={item.status}
+          onClickAction={handleClick}
         />
       ))}
     </>
