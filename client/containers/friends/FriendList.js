fad302dbfc1e83fb1fa4e5187e9e992d7c3455ba
KhoiNguyen <thorwaitson@gmail.com>
2021-03-21 02:57:32 -0500

    add getting friend list

--- client/containers/friends/FriendList.js
+++ client/containers/friends/FriendList.js
@@ -1,24 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import FriendTag from "../../components/friends/friend_tag";
+import { fetchFriendList } from "../../utils/Friends";
+import utilStyles from "../../styles/utils.module.css";
 
 export default function FriendList() {
-  const [friendList, setFriendList] = useState([
-    {
-      username: "Placeholder",
-      email: "holder@holder.com",
-      avatarURL: "/img/avatar.png",
-    },
-  ]);
+  const [friendList, setFriendList] = useState([]);
+  useEffect(() => {
+    fetchFriendList().then((r) => setFriendList(r));
+  }, []);
 
-  return (
+  return friendList.length !== 0 ? (
     <>
       {friendList.map((item) => (
         <FriendTag
-          username={item.username}
-          email={item.email}
-          avatarURL={item.avatarURL}
+          username={item.friend.username}
+          email={item.friend.email}
+          avatarURL={item.friend.avatarURL}
         />
       ))}
     </>
+  ) : (
+    <p className={utilStyles.responseText}>You have no friends yet</p>
   );
 }
--- client/utils/Friends.js
+++ client/utils/Friends.js
@@ -2,7 +2,14 @@ import { origin } from "../config/config";
 
 module.exports = {
   async fetchFriendList() {
-    const friendList = [];
-    return friendList;
+    try {
+      const res = await fetch(
+        `${origin}/api/friends?userEmail=${localStorage.getItem("email")}`
+      );
+      const friendList = await res.json();
+      return friendList;
+    } catch (err) {
+      console.error("Error getting friend list");
+    }
   },
 };
