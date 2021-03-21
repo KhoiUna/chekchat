4b804e47a0e01192ea316ee80cd36e627237c5fa
KhoiNguyen <thorwaitson@gmail.com>
2021-03-20 22:49:36 -0500

    update client utils

---
+++ client/utils/FriendRequest.js
@@ -0,0 +1,47 @@
+import { origin } from "../config/config";
+
+module.exports = {
+  async fetchSentFriendRequestsList() {
+    try {
+      const res = await fetch(
+        `${origin}/api/friends/requests/sent?userEmail=${localStorage.getItem(
+          "email"
+        )}`
+      );
+      const friendRequestList = await res.json();
+      return friendRequestList;
+    } catch (err) {
+      console.error("Error getting friend request list");
+    }
+  },
+  async fetchReceivedFriendRequestsList() {
+    try {
+      const res = await fetch(
+        `${origin}/api/friends/requests/received?userEmail=${localStorage.getItem(
+          "email"
+        )}`
+      );
+      const friendRequestList = await res.json();
+      return friendRequestList;
+    } catch (err) {
+      console.error("Error getting friend request list");
+    }
+  },
+  async sendFriendRequest(requestEmail) {
+    try {
+      const res = await fetch(`${origin}/api/friends/requests`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userEmail: localStorage.getItem("email"),
+          requestEmail,
+        }),
+      });
+      return res;
+    } catch (err) {
+      console.error("Error sending friend request");
+    }
+  },
+};
--- client/utils/Friends.js
+++ client/utils/Friends.js
@@ -5,34 +5,4 @@ module.exports = {
     const friendList = [];
     return friendList;
   },
-  async fetchFriendRequestsList() {
-    try {
-      const res = await fetch(
-        `${origin}/api/friends/requests?userEmail=${localStorage.getItem(
-          "email"
-        )}`
-      );
-      const friendRequestList = await res.json();
-      return friendRequestList;
-    } catch (err) {
-      console.error("Error getting friend request list");
-    }
-  },
-  async sendFriendRequest(requestEmail) {
-    try {
-      const res = await fetch(`${origin}/api/friends/requests`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          userEmail: localStorage.getItem("email"),
-          requestEmail,
-        }),
-      });
-      return res;
-    } catch (err) {
-      console.error("Error sending friend request");
-    }
-  },
 };
