b19c0882feae3e9e21847d0e0cdc6612263df6ef
KhoiNguyen <thorwaitson@gmail.com>
2021-03-26 13:36:19 -0500

    update assign UI

---
+++ client/components/missions/mission_request.js
@@ -0,0 +1,100 @@
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import Badge from "@material-ui/core/Badge";
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 270,
+  },
+  media: {
+    height: 20,
+  },
+  gridColumn: {
+    display: "grid",
+    gridTemplateColumns: "1fr 0fr 1fr",
+  },
+  gridRow: {
+    display: "grid",
+    gridTemplateRows: "1fr 1fr",
+  },
+  overflowText: {
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+    whiteSpace: "nowrap",
+  },
+  overflowUsername: {
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+    whiteSpace: "nowrap",
+    padding: "0 0.5rem",
+  },
+  divider: {
+    margin: "0.7rem 0 0.7rem 0",
+  },
+  badge: {
+    margin: "0.8rem 0",
+  },
+});
+
+export default function MissionRequest({ status, username, subject }) {
+  const classes = useStyles();
+
+  return (
+    <Badge
+      badgeContent={status === "Pending" ? `${status}...` : status}
+      color="primary"
+      anchorOrigin={{
+        vertical: "bottom",
+        horizontal: "left",
+      }}
+      className={classes.badge}
+    >
+      <Card className={classes.root}>
+        <CardActionArea className={classes.gridColumn}>
+          <div className={classes.gridRow}>
+            <Typography
+              gutterBottom
+              variant="inherit"
+              component="h2"
+              className={classes.overflowText}
+            >
+              To:
+            </Typography>
+            <Typography
+              gutterBottom
+              variant="h6"
+              component="h2"
+              className={classes.overflowUsername}
+            >
+              {username}
+            </Typography>
+          </div>
+
+          <Divider
+            orientation="vertical"
+            flexItem
+            className={classes.divider}
+          />
+
+          <CardContent className={classes.gridRow}>
+            <Typography gutterBottom variant="inherit" component="h2">
+              Subject:
+            </Typography>
+            <Typography
+              gutterBottom
+              variant="h6"
+              component="h2"
+              className={classes.overflowText}
+            >
+              {subject || "Design logo"}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Badge>
+  );
+}
--- client/pages/assigning.js
+++ client/pages/assigning.js
@@ -1,9 +1,132 @@
 import MainLayout from "../containers/main_layout";
+import MissionRequest from "../components/missions/mission_request";
+import Grid from "@material-ui/core/Grid";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
+import {
+  createMuiTheme,
+  makeStyles,
+  MuiThemeProvider,
+} from "@material-ui/core/styles";
+import HUE from "@material-ui/core/colors/blue";
+import Typography from "@material-ui/core/Typography";
+import { useEffect, useState } from "react";
+
+const useStyles = makeStyles((theme) => ({
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+  button: {
+    position: "fixed",
+    bottom: "1rem",
+    right: "0.8rem",
+    zIndex: 1,
+  },
+  badge: {
+    margin: "0.8rem 0",
+  },
+  alignLeft: {
+    textAlign: "left",
+    margin: "0.4rem 0 0.5rem 1rem",
+  },
+}));
+
+const buttonTheme = createMuiTheme({
+  palette: {
+    primary: { main: HUE[500], contrastText: "white" },
+  },
+});
 
 export default function Assigning() {
+  const classes = useStyles();
+
+  const [sentMissionList, setSentMissionList] = useState([]);
+  useEffect(() => {
+    setSentMissionList([
+      {
+        subject: "Logo design",
+        due_date: new Date(),
+        from: { username: "Holder holderabc", avatarURL: "/img/avatar.png" },
+        to: { username: "Holder holder", avatarURL: "/img/avatar.png" },
+        description: "",
+        status: "Pending",
+      },
+      {
+        subject: "Logo design",
+        due_date: new Date(),
+        from: { username: "Holder holderabc", avatarURL: "/img/avatar.png" },
+        to: { username: "Holder holder", avatarURL: "/img/avatar.png" },
+        description: "",
+        status: "Pending",
+      },
+      {
+        subject: "Logo design",
+        due_date: new Date(),
+        from: { username: "Holder holderabc", avatarURL: "/img/avatar.png" },
+        to: { username: "Holder holder", avatarURL: "/img/avatar.png" },
+        description: "",
+        status: "Pending",
+      },
+      {
+        subject: "Logo design",
+        due_date: new Date(),
+        from: { username: "Holder holderabc", avatarURL: "/img/avatar.png" },
+        to: { username: "Holder holder", avatarURL: "/img/avatar.png" },
+        description: "",
+        status: "Pending",
+      },
+      {
+        subject: "Logo design",
+        due_date: new Date(),
+        from: { username: "Holder holderabc", avatarURL: "/img/avatar.png" },
+        to: { username: "Holder holder", avatarURL: "/img/avatar.png" },
+        description: "",
+        status: "Pending",
+      },
+      {
+        subject: "Logo design",
+        due_date: new Date(),
+        from: { username: "Holder holderabc", avatarURL: "/img/avatar.png" },
+        to: { username: "Holder holder", avatarURL: "/img/avatar.png" },
+        description: "",
+        status: "Pending",
+      },
+    ]);
+  }, []);
+
   return (
     <MainLayout componentName="Assigning">
-      <p>Assigning</p>
+      <Typography
+        gutterBottom
+        variant="inherit"
+        component="h2"
+        className={classes.alignLeft}
+      >
+        Sent mission:
+      </Typography>
+
+      <Grid container direction="column" justify="center" alignItems="center">
+        {sentMissionList.map((i) => (
+          <MissionRequest
+            status={i.status}
+            username={i.from.username}
+            subject={i.subject}
+            avatarURL={i.from.avatarURL}
+          />
+        ))}
+      </Grid>
+
+      <MuiThemeProvider theme={buttonTheme}>
+        <Fab
+          color="primary"
+          aria-label="assign mission"
+          variant="extended"
+          className={classes.button}
+        >
+          <AddIcon className={classes.extendedIcon} />
+          Assign
+        </Fab>
+      </MuiThemeProvider>
     </MainLayout>
   );
 }
