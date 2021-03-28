7336803437b8d106d106ff2d19078bb037905bfe
KhoiNguyen <thorwaitson@gmail.com>
2021-03-28 13:55:10 -0500

    update missionRouter update mission request

--- server/routes/missionRouter.js
+++ server/routes/missionRouter.js
@@ -76,10 +76,10 @@ router.post("/", async (req, res, next) => {
 router.put("/", async (req, res, next) => {
   try {
     const { action, requestId } = req.body;
-    console.log(action, requestId);
+
     //Update mission request
-    // if (!(await Missions.updateRequest(requestId, action)))
-    //   return res.status(400).send("Sorry, something is wrong");
+    if (!(await Missions.updateRequest(requestId, action)))
+      return res.status(400).send("Sorry, something is wrong");
 
     res.send("ok");
   } catch (err) {
