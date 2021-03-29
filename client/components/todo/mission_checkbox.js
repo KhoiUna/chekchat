1135330ae37981cfe6757042a456b25040645ee2
KhoiNguyen <thorwaitson@gmail.com>
2021-03-28 22:24:43 -0500

    add mission checkbox for todo page UI

---
+++ client/components/todo/mission_checkbox.js
@@ -0,0 +1,119 @@
+import {
+  createMuiTheme,
+  MuiThemeProvider,
+  makeStyles,
+} from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import Checkbox from "@material-ui/core/Checkbox";
+import { useState } from "react";
+
+const useStyles = makeStyles({
+  root: {
+    width: "95%",
+    maxWidth: 600,
+    margin: "1rem 0",
+  },
+  gridColumn: {
+    display: "grid",
+    gridTemplateColumns: "1fr 0fr 5fr",
+  },
+  gridRow: {
+    display: "grid",
+    gridTemplateRows: "1fr 1fr",
+    textAlign: "left",
+    margin: 0,
+  },
+  overflowText: {
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+    whiteSpace: "nowrap",
+    margin: 0,
+  },
+  divider: {
+    margin: "0.7rem 0 0.7rem 0",
+  },
+  checkbox: {
+    backgroundColor: "transparent",
+  },
+  due_date: {
+    marginTop: "0.5rem",
+  },
+});
+
+const theme = createMuiTheme({
+  props: {
+    MuiButtonBase: {
+      disableRipple: true,
+    },
+  },
+});
+
+export default function MissionCheckbox({
+  completed,
+  subject,
+  due_date,
+  username,
+}) {
+  const classes = useStyles();
+
+  const [checked, setChecked] = useState(completed);
+  const handleChange = ({ target }) => {
+    setChecked(target.checked);
+  };
+
+  return (
+    <MuiThemeProvider theme={theme}>
+      <Card className={classes.root}>
+        <CardActionArea className={classes.gridColumn}>
+          <Checkbox
+            checked={checked}
+            onChange={handleChange}
+            color="primary"
+            inputProps={{ "aria-label": "checkbox" }}
+            className={classes.checkbox}
+          />
+
+          <Divider
+            orientation="vertical"
+            flexItem
+            className={classes.divider}
+          />
+
+          <CardContent className={classes.gridRow}>
+            <Typography
+              gutterBottom
+              variant="h6"
+              className={classes.overflowText}
+            >
+              <b>From:</b> {username}
+            </Typography>
+
+            <Typography
+              gutterBottom
+              variant="h6"
+              className={classes.overflowText}
+            >
+              <b>Subject:</b> {subject}
+            </Typography>
+
+            <Typography
+              variant="body2"
+              gutterBottom
+              className={classes.due_date}
+            >
+              <b>Due: </b>
+              {new Date(due_date).toLocaleDateString()}
+              <span style={{ marginLeft: "0.3rem" }}>
+                {new Date(due_date).toLocaleTimeString()}
+              </span>
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </MuiThemeProvider>
+  );
+}
