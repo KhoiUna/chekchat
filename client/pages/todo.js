e2c53cf893017202cd6b50d92ed96cce1a91c407
KhoiNguyen <thorwaitson@gmail.com>
2021-03-28 22:31:58 -0500

    update todo page in  client

--- client/pages/todo.js
+++ client/pages/todo.js
@@ -1,10 +1,27 @@
-import { origin } from "../config/config";
 import MainLayout from "../containers/main_layout";
+import MissionCheckbox from "../components/todo/mission_checkbox";
+import Grid from "@material-ui/core/Grid";
+import { useEffect, useState } from "react";
+import { fetchMissionTodoList } from "../utils/Missions";
 
 export default function Todo() {
+  const [missionTodoList, setMissionTodoList] = useState([]);
+  useEffect(() => {
+    fetchMissionTodoList().then((r) => setMissionTodoList(r));
+  }, []);
+
   return (
     <MainLayout componentName="Todo">
-      <div>Todo</div>
+      <Grid container direction="column" justify="center" alignItems="center">
+        {missionTodoList.map((i) => (
+          <MissionCheckbox
+            completed={i.completed}
+            subject={i.subject}
+            due_date={i.due_date}
+            username={i.from.username}
+          />
+        ))}
+      </Grid>
     </MainLayout>
   );
 }
