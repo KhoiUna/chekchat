86820e73b8cbb226c0d7bf4191dd8dbff3e31737
KhoiNguyen <thorwaitson@gmail.com>
2021-04-04 19:59:35 -0500

    install redux

---
+++ client/components/send/filter_button.js
@@ -0,0 +1,94 @@
+import { useState } from "react";
+import {
+  withStyles,
+  createMuiTheme,
+  MuiThemeProvider,
+} from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import SortIcon from "@material-ui/icons/Sort";
+import utilsStyle from "../../styles/utils.module.css";
+import HUE from "@material-ui/core/colors/blue";
+
+const StyledMenu = withStyles({
+  paper: {
+    border: "1px solid #d3d4d5",
+  },
+})((props) => (
+  <Menu
+    elevation={0}
+    getContentAnchorEl={null}
+    anchorOrigin={{
+      vertical: "bottom",
+      horizontal: "right",
+    }}
+    transformOrigin={{
+      vertical: "top",
+      horizontal: "center",
+    }}
+    {...props}
+  />
+));
+
+const StyledMenuItem = withStyles((theme) => ({
+  root: {
+    "&:focus": {
+      backgroundColor: "#f1f1f1",
+      "& .MuiListItemIcon-root, & .MuiListItemText-primary": {
+        color: HUE[500],
+      },
+    },
+  },
+}))(MenuItem);
+
+const buttonTheme = createMuiTheme({
+  palette: {
+    primary: { main: HUE[500], contrastText: "white" },
+  },
+});
+
+export default function FilterButton({}) {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const handleClick = ({ currentTarget }) => {
+    setAnchorEl(currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div className={utilsStyle.sortButton}>
+      <MuiThemeProvider theme={buttonTheme}>
+        <Button
+          aria-controls="sort menu"
+          aria-haspopup="true"
+          variant="contained"
+          color="primary"
+          onClick={handleClick}
+        >
+          <b>Sort by</b> <SortIcon style={{ marginLeft: "0.3rem" }} />
+        </Button>
+      </MuiThemeProvider>
+
+      <StyledMenu
+        id="customized-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <StyledMenuItem>
+          <ListItemText primary="Due date" />
+        </StyledMenuItem>
+        <StyledMenuItem>
+          <ListItemText primary="... ..." />
+        </StyledMenuItem>
+        <StyledMenuItem>
+          <ListItemText primary="... ..." />
+        </StyledMenuItem>
+      </StyledMenu>
+    </div>
+  );
+}
