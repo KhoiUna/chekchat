bb0c8f896388dbaf0675e506b5153dbe6de47152
KhoiNguyen <thorwaitson@gmail.com>
2021-04-04 21:52:43 -0500

    update calendar page

--- client/pages/calendar.js
+++ client/pages/calendar.js
@@ -3,7 +3,9 @@ import MainLayout from "../containers/main_layout";
 export default function Calendar() {
   return (
     <MainLayout componentName="Calendar">
-      <p>Calendar</p>
+      <h3>
+        <i>Calendar is not available yet!</i>
+      </h3>
     </MainLayout>
   );
 }
