103c0b846cb568333be6aac84e119ddaeb102b0c
KhoiNguyen <thorwaitson@gmail.com>
2021-04-07 14:44:59 -0500

    add Notifications util for client

---
+++ client/utils/Notifications.js
@@ -0,0 +1,13 @@
+module.exports = {
+  async fetchNotificationsList() {
+    try {
+      const res = await fetch(
+        `${origin}/api/notifications?userEmail=${localStorage.getItem("email")}`
+      );
+      const notificationList = await res.json();
+      return notificationList;
+    } catch (err) {
+      console.error("Error getting notifications list");
+    }
+  },
+};
