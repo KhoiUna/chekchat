d1e6ac870254df75100820d76a43e1958e801613
KhoiNguyen <thorwaitson@gmail.com>
2021-05-15 11:08:16 -0500

    change settings page to feedback page

---
+++ client/pages/feedback.js
@@ -0,0 +1,9 @@
+import MainLayout from "../containers/main_layout";
+
+export default function Feedback() {
+  return (
+    <MainLayout componentName="Feedback">
+      <p>Feedback</p>
+    </MainLayout>
+  );
+}
