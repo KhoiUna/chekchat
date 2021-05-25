c5b50ecc7988cfa080e30e7c45fe7338c64f021c
KhoiNguyen <thorwaitson@gmail.com>
2021-05-25 03:09:05 -0500

    fix pages

--- client/containers/main_layout.js
+++ client/containers/main_layout.js
@@ -6,6 +6,7 @@ import Chat from "../components/chat";
 import { AppBar, Toolbar, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { useDispatch, useSelector } from "react-redux";
+import { loadUserInfoAsync, selectUserInfo } from "../features/userSlice";
 import {
   selectNotificationCount,
   loadNotificationCountAsync,
