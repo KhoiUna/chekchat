bff7b1fc5041b060eb8e253e54239f5555fd417f
KhoiNguyen <thorwaitson@gmail.com>
2021-05-26 00:14:34 -0400

    add custom error page

---
+++ client/pages/_error.js
@@ -0,0 +1,31 @@
+import Layout from "../containers/layout";
+import Link from "next/link";
+import utilStyles from "../styles/utils.module.css";
+
+function Error({ statusCode }) {
+  return (
+    <Layout componentName="Login">
+      <div className={utilStyles.container}>
+        <Typography
+          variant="h6"
+          color="textSecondary"
+          style={{ marginTop: "10%" }}
+        >
+          An {statusCode} error occurs. Please try refreshing the page or head
+          back to our{" "}
+          <Link href="/login">
+            <u>login</u>
+          </Link>{" "}
+          page
+        </Typography>
+      </div>
+    </Layout>
+  );
+}
+
+Error.getInitialProps = ({ res, err }) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  return { statusCode };
+};
+
+export default Error;
