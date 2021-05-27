66f6d5e60c1033d02833a017f78dcafe7e9515b2
KhoiNguyen <thorwaitson@gmail.com>
2021-05-26 23:11:17 -0400

    update logoutRouter in server to destroy session when logged out

--- server/routes/logoutRouter.js
+++ server/routes/logoutRouter.js
@@ -2,6 +2,11 @@ const router = require("express").Router();
 
 router.delete("/", (req, res, next) => {
   try {
+    //Destroy the current session
+    req.session.destroy((err) => {
+      console.error("Cannot destroy session");
+    });
+
     res.clearCookie("sid", { path: "/" }).send("ok");
   } catch (err) {
     console.error("Error logging out user");
