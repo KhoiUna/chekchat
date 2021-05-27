ba76bb7fd7868cbc3a03b076e7c7b78658f5f7c6
KhoiNguyen <thorwaitson@gmail.com>
2021-05-27 00:27:12 -0400

    update server SocketHelper

--- server/helpers/SocketHelper.js
+++ server/helpers/SocketHelper.js
@@ -3,19 +3,7 @@ let users = [];
 module.exports = {
   subscribeUsers(socketId, userEmail) {
     const user = { socketId, userEmail };
-
-    if (users.length === 0) {
-      users.push(user);
-    }
-
-    if (users.some((i) => i?.userEmail === userEmail)) {
-      users = [...users].map((i) => {
-        if (i.userEmail === userEmail) i.socketId = socketId;
-        return i;
-      });
-    } else {
-      users.push(user);
-    }
+    users.push(user);
 
     return user;
   },
