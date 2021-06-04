f57b3c97865e5894b7817722f7e0a61046be6999
KhoiNguyen <thorwaitson@gmail.com>
2021-06-04 01:32:07 -0500

    add passport local config to server

---
+++ server/config/local-passport-setup.js
@@ -0,0 +1,25 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const UsersUtil = require("../utils/UsersUtil");
+
+// Configure the local strategy for use by Passport
+passport.use(
+  new LocalStrategy(
+    { usernameField: "email", passwordField: "password" },
+    async (email, password, done) => {
+      try {
+        const userObj = await UsersUtil.loginUser(email, password);
+        if (!userObj) {
+          return done(null, false);
+        }
+
+        return done(null, {
+          username: userObj.username,
+          email: userObj.email,
+        });
+      } catch (err) {
+        return done(err);
+      }
+    }
+  )
+);
