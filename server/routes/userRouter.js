8ae3685888ac6acc6872ad984862878c1c3f3fae
KhoiNguyen <thorwaitson@gmail.com>
2021-06-04 02:22:12 -0500

    update userRouter in server

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,38 +1,65 @@
 const router = require("express").Router();
 const UsersController = require("../controllers/users.controller");
+const passport = require("passport");
+const redirectMain = require("../middlewares/redirectMain");
+const redirectLogin = require("../middlewares/redirectLogin");
 
 //register, login & logout route
 router.post("/register", UsersController.register);
-router.post("/login", UsersController.login);
-router.delete("/logout", UsersController.logout);
+router.post(
+  "/login",
+  redirectMain,
+  passport.authenticate("local", {
+    successRedirect: "/api/user",
+    failureRedirect: "/api/user/login",
+  })
+);
+router.delete("/logout", redirectLogin, UsersController.logout);
 
 //user route
-router.get("/", UsersController.getUserInfo);
+router.get("/", redirectLogin, UsersController.getUserInfo);
 
 //notification route
-router.get("/notifications", UsersController.getNotifications);
-router.get("/notifications/bell", UsersController.getNotificationCount);
+router.get("/notifications", redirectLogin, UsersController.getNotifications);
+router.get(
+  "/notifications/bell",
+  redirectLogin,
+  UsersController.getNotificationCount
+);
 
 //mission route
-router.get("/missions", UsersController.getMissionRequest);
-router.get("/missions/:requestId", UsersController.getMissionInfo);
-router.post("/missions", UsersController.sendMissionRequest);
-router.put("/missions", UsersController.updateMissionVisibility);
-router.delete("/missions", UsersController.deletePendingMission);
+router.get("/missions", redirectLogin, UsersController.getMissionRequest);
+router.get(
+  "/missions/:requestId",
+  redirectLogin,
+  UsersController.getMissionInfo
+);
+router.post("/missions", redirectLogin, UsersController.sendMissionRequest);
+router.put("/missions", redirectLogin, UsersController.updateMissionVisibility);
+router.delete("/missions", redirectLogin, UsersController.deletePendingMission);
 
 //todo route
-router.get("/todo", UsersController.getMissionTodoList);
+router.get("/todo", redirectLogin, UsersController.getMissionTodoList);
 
 //friend route
-router.get("/friends/requests/sent", UsersController.getSentFriendRequestList);
+router.get(
+  "/friends/requests/sent",
+  redirectLogin,
+  UsersController.getSentFriendRequestList
+);
 router.get(
   "/friends/requests/received",
+  redirectLogin,
   UsersController.getReceivedFriendRequestList
 );
-router.post("/friends/requests", UsersController.sendFriendRequest);
-router.get("/friends", UsersController.getFriendList);
+router.post(
+  "/friends/requests",
+  redirectLogin,
+  UsersController.sendFriendRequest
+);
+router.get("/friends", redirectLogin, UsersController.getFriendList);
 
 //feedback route
-router.post("/feedback", UsersController.postFeedback);
+router.post("/feedback", redirectLogin, UsersController.postFeedback);
 
 module.exports = router;
