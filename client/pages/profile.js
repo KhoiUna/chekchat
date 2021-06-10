70fdf5148a87c49ee15fb0049dc453ebfbef9944
KhoiNguyen <thorwaitson@gmail.com>
2021-06-10 00:22:06 -0500

    update profile page to update user avatar

--- client/pages/profile.js
+++ client/pages/profile.js
@@ -1,9 +1,128 @@
 import MainLayout from "../containers/main_layout";
+import Snackbar from "@material-ui/core/Snackbar";
+import Button from "@material-ui/core/Button";
+import { buttonTheme } from "../themes/theme";
+import { IKContext, IKUpload } from "imagekitio-react";
+import { origin } from "../config/config";
+import { makeStyles, MuiThemeProvider } from "@material-ui/core/styles";
+import utilStyles from "../styles/utils.module.css";
+import MuiAlert from "@material-ui/lab/Alert";
+import TextField from "@material-ui/core/TextField";
+import { useDispatch, useSelector } from "react-redux";
+import { selectUserInfo, updateAvatarURL } from "../features/userSlice";
+import UsersUtil from "../utils/UsersUtil";
+import { useState } from "react";
 
-export default function Profile({}) {
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+const useStyles = makeStyles((theme) => ({
+  textField: {
+    margin: "2rem 0 2rem 0",
+  },
+}));
+
+export default function Profile() {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  const { username, avatarURL } = useSelector(selectUserInfo);
+
+  const [open, setOpen] = useState(false);
+  const openSnackbar = () => {
+    setOpen(true);
+  };
+  const closeSnackbar = (e, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
+  };
+
+  const onError = (err) => {
+    console.error("Error", err);
+  };
+
+  const [snackbarText, setSnackbarText] = useState("");
+  const onSuccess = async (res) => {
+    console.log("Success", res);
+
+    if (res.fileType === "image") {
+      const avatarPath = res.url;
+      if (await UsersUtil.updateUserAvatar(avatarPath)) {
+        dispatch(updateAvatarURL(avatarPath));
+        setSnackbarText("Avatar is successfully updated!");
+        openSnackbar();
+      }
+    } else {
+      // setImgURL("");
+    }
+  };
+
   return (
     <MainLayout componentName="Profile">
-      <p>Profile</p>
+      <Snackbar
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        key={"top" + "center"}
+        open={open}
+        onClose={closeSnackbar}
+        autoHideDuration={3000}
+      >
+        <Alert severity="success">{snackbarText}</Alert>
+      </Snackbar>
+
+      <img
+        src={avatarURL ? avatarURL + "?tr=w-300,h-300" : null}
+        className={utilStyles.uploadedAvatar}
+        alt={username}
+      />
+      <br />
+
+      <IKContext
+        publicKey={process.env.NEXT_PUBLIC_IMGKIT_PUBLIC_KEY}
+        urlEndpoint={process.env.NEXT_PUBLIC_IMGKIT_IMGKIT_URL_ENDPOINT}
+        authenticationEndpoint={`${origin}/api/user/updateAvatar/auth`}
+      >
+        <MuiThemeProvider theme={buttonTheme}>
+          <Button color="primary">
+            Change avatar
+            <IKUpload
+              fileName="user_avatar.png"
+              onError={onError}
+              onSuccess={onSuccess}
+              folder={"/chekchat_upload"}
+              style={{ position: "absolute" }}
+            />
+          </Button>
+        </MuiThemeProvider>
+      </IKContext>
+      <br />
+
+      <TextField
+        required
+        id="username"
+        name="username"
+        label="Username"
+        autoComplete="off"
+        variant="filled"
+        className={classes.textField}
+        value={username}
+        // onChange={}
+      />
+      <br />
+
+      <MuiThemeProvider theme={buttonTheme}>
+        <Button
+          variant="contained"
+          color="primary"
+          className={utilStyles.button}
+          type="submit"
+          // onClick={handleClick}
+        >
+          Update
+        </Button>
+      </MuiThemeProvider>
     </MainLayout>
   );
 }
