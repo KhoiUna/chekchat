aca2c0029f7f198dab96a4f1364b7bbe32668ccf
KhoiNguyen <thorwaitson@gmail.com>
2021-06-16 12:02:28 -0500

    remove @svgr/webpack from package.json

--- client/next.config.js
+++ client/next.config.js
@@ -4,12 +4,4 @@ module.exports = {
     // loader: "cloudinary",
     domains: ["ik.imagekit.io"],
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-
-    return config;
-  },
 };
