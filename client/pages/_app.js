836eeb76b0d1b8d7f7b25f60d5111ed3addfd94e
KhoiNguyen <thorwaitson@gmail.com>
2021-06-26 01:16:33 -0500

    add service worker for PWA

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,8 +1,17 @@
 import "../styles/global.css";
 import { wrapper } from "../app/store";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <script type="module" src="/pwabuilder-sw-register.js"></script>
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default wrapper.withRedux(MyApp);
