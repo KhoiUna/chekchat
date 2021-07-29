3d95c75a691be8e52a96af4e90ece6a3403fcd4d
KhoiNguyen <thorwaitson@gmail.com>
2021-07-29 12:58:30 -0500

    update sending bar UI

--- client/components/chat/send_bar.js
+++ client/components/chat/send_bar.js
@@ -1,12 +1,13 @@
 import SendIcon from "@material-ui/icons/Send";
+import IconButton from "@material-ui/core/IconButton";
 import utilStyles from "../../styles/utils.module.css";
-import Fab from "@material-ui/core/Fab";
 import { makeStyles, MuiThemeProvider } from "@material-ui/core/styles";
 import { buttonTheme } from "../../themes/theme";
 
 const useStyles = makeStyles({
   button: {
-    margin: "auto",
+    margin: "0 auto auto auto",
+    padding: "0.4rem",
   },
 });
 
@@ -14,21 +15,23 @@ export default function SendBar({}) {
   const classes = useStyles();
   return (
     <div className={utilStyles.send_bar}>
-      <div
-        className={utilStyles.send_form}
-        placeholder="Type message"
-        contentEditable={true}
-      ></div>
+      <div className={utilStyles.send_area}>
+        <div
+          className={utilStyles.send_form}
+          placeholder="Type message"
+          contentEditable={true}
+        ></div>
 
-      <MuiThemeProvider theme={buttonTheme}>
-        <Fab
-          color="primary"
-          aria-label="Send message"
-          className={classes.button}
-        >
-          <SendIcon style={{ fontSize: "2rem" }} />
-        </Fab>
-      </MuiThemeProvider>
+        <MuiThemeProvider theme={buttonTheme}>
+          <IconButton
+            color="primary"
+            aria-label="Send message"
+            className={classes.button}
+          >
+            <SendIcon style={{ fontSize: "2rem" }} />
+          </IconButton>
+        </MuiThemeProvider>
+      </div>
     </div>
   );
 }
