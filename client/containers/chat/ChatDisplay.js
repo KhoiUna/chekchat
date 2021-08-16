8211b8ea346997a5f7f140c7b023fa0c10b40ccc
KhoiNguyen <thorwaitson@gmail.com>
2021-08-16 17:08:40 -0500

    add a blank state for ChatDisplay.js in client

--- client/containers/chat/ChatDisplay.js
+++ client/containers/chat/ChatDisplay.js
@@ -1,6 +1,20 @@
 import Chatbox from "../../components/chat/chatbox";
+import Typography from "@material-ui/core/Typography";
 
 export default function ChatDisplay({ msgArray }) {
+  if (msgArray.length === 0) {
+    return (
+      <Typography
+        variant="h6"
+        color="textSecondary"
+        style={{ marginTop: "10%" }}
+        component="p"
+      >
+        <i>Start filling your conversation with productivity!</i>
+      </Typography>
+    );
+  }
+
   return msgArray.map((i, index) => {
     return (
       <Chatbox
