88523cabd28033a8849ec26208bb073e97744ffb
KhoiNguyen <thorwaitson@gmail.com>
2021-08-20 18:40:49 -0500

    add date divider for chat room

--- client/containers/chat/ChatDisplay.js
+++ client/containers/chat/ChatDisplay.js
@@ -1,7 +1,22 @@
 import Chatbox from "../../components/chat/chatbox";
 import Typography from "@material-ui/core/Typography";
+import FormatDatetime from "../../helpers/FormatDatetime";
+import utilStyles from "../../styles/utils.module.css";
+
+const DateDivider = ({ time }) => (
+  <div className={utilStyles.datedivider_container}>
+    <div className={utilStyles.datedivider_info}>
+      <Typography variant="subtitle1" gutterBottom style={{ margin: 0 }}>
+        <b>{FormatDatetime.forDateDivider(time)}</b>
+      </Typography>
+    </div>
+  </div>
+);
 
 export default function ChatDisplay({ msgArray }) {
+  let repeatedDate;
+  let renderDateDivider;
+
   if (msgArray.length === 0) {
     return (
       <Typography
@@ -16,13 +31,26 @@ export default function ChatDisplay({ msgArray }) {
   }
 
   return msgArray.map((i, index) => {
+    if (new Date(i.sent_datetime).toDateString() !== repeatedDate) {
+      repeatedDate = new Date(i.sent_datetime).toDateString();
+      renderDateDivider = true;
+    } else {
+      renderDateDivider = false;
+    }
+
     return (
-      <Chatbox
-        key={index}
-        from_user={i.from_user}
-        message={i.message}
-        sent_datetime={i.sent_datetime}
-      />
+      <>
+        {renderDateDivider && (
+          <DateDivider key={index} time={i.sent_datetime} />
+        )}
+
+        <Chatbox
+          key={i._id}
+          from_user={i.from_user}
+          message={i.message}
+          sent_datetime={i.sent_datetime}
+        />
+      </>
     );
   });
 }
