5d231e77cefb61b11d577160bff7389c8e1a19bd
KhoiNguyen <thorwaitson@gmail.com>
2021-08-20 23:06:26 -0500

    add cookieOptions to client config.js

--- client/config/config.js
+++ client/config/config.js
@@ -1,4 +1,11 @@
 module.exports = {
   origin: process.env.NEXT_PUBLIC_ORIGIN || "https://api.chekchat.xyz",
   // "https://chekapp.herokuapp.com",
+  cookieOptions: {
+    path: "/",
+    expires: new Date(new Date().getTime() + 30 * 24 * 60 * 60 * 1000), // 30 days
+    maxAge: 24 * 60 * 60, // 1 day
+    secure: true,
+    sameSite: "strict",
+  },
 };
