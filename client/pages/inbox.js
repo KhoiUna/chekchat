35e7786b52a799a50d25701e9e5965f95bbc778f
KhoiNguyen <thorwaitson@gmail.com>
2021-08-20 23:34:44 -0500

    update inbox UI

--- client/pages/inbox.js
+++ client/pages/inbox.js
@@ -1,67 +1,22 @@
 import MainLayout from "../containers/main_layout";
-import { makeStyles, MuiThemeProvider } from "@material-ui/core/styles";
-import Tabs from "@material-ui/core/Tabs";
 import Grid from "@material-ui/core/Grid";
-import Tab from "@material-ui/core/Tab";
-import { useState } from "react";
-import ReceivedFriendRequestList from "../containers/inbox/ReceivedFriendRequestList";
 import PendingMissionList from "../containers/inbox/PendingMissionList";
 import RefreshButton from "../components/refresh_button";
-import { buttonTheme } from "../themes/theme";
-
-const useStyles = makeStyles({
-  tabTitle: {
-    fontWeight: "bold",
-    fontSize: "1rem",
-  },
-});
 
 export default function Inbox({}) {
-  const classes = useStyles();
-
-  const [value, setValue] = useState(0);
-  const handleTabChange = (e, newValue) => {
-    setValue(newValue);
-  };
-
   return (
     <MainLayout componentName="Inbox">
       <RefreshButton />
 
-      <MuiThemeProvider theme={buttonTheme}>
-        <Tabs
-          value={value}
-          onChange={handleTabChange}
-          indicatorColor="primary"
-          textColor="primary"
-          centered
-        >
-          <Tab label="Task requests" className={classes.tabTitle} />
-          <Tab label="Friend requests" className={classes.tabTitle} />
-        </Tabs>
-      </MuiThemeProvider>
-
-      {value === 0 ? (
-        <Grid
-          container
-          direction="column"
-          justifyContent="center"
-          alignItems="center"
-          style={{ marginBottom: "5rem" }}
-        >
-          <PendingMissionList />
-        </Grid>
-      ) : (
-        <Grid
-          container
-          direction="column"
-          justifyContent="center"
-          alignItems="center"
-          style={{ marginBottom: "5rem" }}
-        >
-          <ReceivedFriendRequestList />
-        </Grid>
-      )}
+      <Grid
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        style={{ marginBottom: "5rem" }}
+      >
+        <PendingMissionList />
+      </Grid>
     </MainLayout>
   );
 }
