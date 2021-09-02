4399e23aaeb510e4484b6672a1d256750b3012ef
KhoiNguyen <thorwaitson@gmail.com>
2021-09-02 13:04:31 -0500

    add google tag manager

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -7,6 +7,21 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          {/* Google Tag Manager */}
+          <script>
+            {(function (w, d, s, l, i) {
+              w[l] = w[l] || [];
+              w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+              var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != "dataLayer" ? "&l=" + l : "";
+              j.async = true;
+              j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+              f.parentNode.insertBefore(j, f);
+            })(window, document, "script", "dataLayer", "GTM-W6PK98F")}
+          </script>
+          {/* End Google Tag Manager */}
+
           <link rel="icon" href="/favicon.ico" />
           <meta name="theme-color" content="#000000" />
           <meta name="description" content="The app for assigning tasks" />
@@ -26,6 +41,17 @@ export default class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
         </Head>
         <body>
+          {/* Google Tag Manager (noscript) */}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-W6PK98F"
+              height="0"
+              width="0"
+              style="display:none;visibility:hidden"
+            ></iframe>
+          </noscript>
+          {/* End Google Tag Manager (noscript) */}
+
           <Main />
           <NextScript />
         </body>
