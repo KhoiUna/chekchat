e60866bd4c9a974c6ee24072dec403a2b48bfa1b
KhoiNguyen <thorwaitson@gmail.com>
2021-09-02 13:24:36 -0500

    add google tag manager

--- client/containers/home_layout.js
+++ client/containers/home_layout.js
@@ -12,20 +12,21 @@ export default function Home({ children, componentName }) {
     <>
       <Head>
         {/* Google Tag Manager */}
-        <Script strategy="beforeInteractive">
-          {`if (typeof window !== "undefined") {
-    (function (w, d, s, l, i) {
-      w[l] = w[l] || [];
-      w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
-      var f = d.getElementsByTagName(s)[0],
-        j = d.createElement(s),
-        dl = l != "dataLayer" ? "&l=" + l : "";
-      j.async = true;
-      j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
-      f.parentNode.insertBefore(j, f);
-    })(window, document, "script", "dataLayer", "GTM-W6PK98F");
-  }`}
-        </Script>
+        <Script
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `if (typeof window !== "undefined") {(function (w, d, s, l, i) {
+              w[l] = w[l] || [];
+              w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+              var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != "dataLayer" ? "&l=" + l : "";
+              j.async = true;
+              j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+              f.parentNode.insertBefore(j, f);
+            })(window, document, "script", "dataLayer", "GTM-W6PK98F");}`,
+          }}
+        />
         {/* End Google Tag Manager */}
 
         <link rel="icon" href="/favicon.ico" />
