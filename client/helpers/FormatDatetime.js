f99ad5bfd15cf91c74c049fdd4e1a9ee79000b68
KhoiNguyen <thorwaitson@gmail.com>
2021-10-13 00:23:02 -0500

    update client helper FormatDatetime.js

--- client/helpers/FormatDatetime.js
+++ client/helpers/FormatDatetime.js
@@ -103,7 +103,7 @@ module.exports = {
       if (date < 10) {
         date = `0${date}`;
       }
-      if (month < 11) {
+      if (month < 10) {
         month = `0${month}`;
       }
       timeRecordStr = `${time.toString().slice(0, 3)} ${month}/${date}`;
