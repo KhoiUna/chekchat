c484777d9af2a1bc196008f7090fa7961c3d81ca
KhoiNguyen <thorwaitson@gmail.com>
2021-10-22 15:24:51 -0500

    update pricing page

--- client/pages/pricing.js
+++ client/pages/pricing.js
@@ -2,15 +2,42 @@ import HomeLayout from "../containers/home_layout";
 import Typography from "@material-ui/core/Typography";
 import homeStyles from "../styles/home.module.css";
 import { useState } from "react";
+import { origin } from "../config/config";
 
 export default function Pricing({}) {
   const [email, setEmail] = useState("");
+  const [status, setStatus] = useState({ color: "", text: "" });
   const handleChange = ({ target }) => {
+    setStatus({ color: "", text: "" });
     setEmail(target.value);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    try {
+      const res = await fetch(`${origin}/api/requestAccess`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email }),
+      });
+      if (res.ok === true) {
+        setEmail("");
+        return setStatus({
+          color: "#3dda58",
+          text: "You will receive a survey lately in your inbox. Please answer it to get early access.",
+        });
+      }
+
+      return setStatus({
+        color: "red",
+        text: await res.text(),
+      });
+    } catch (err) {
+      console.error("Error submitting email");
+      return;
+    }
   };
 
   return (
@@ -65,9 +92,26 @@ export default function Pricing({}) {
                 placeholder="Enter your email"
                 onChange={handleChange}
               />
+              {status.text && (
+                <p
+                  gutterBottom
+                  style={{
+                    color: status.color,
+                    fontWeight: "bold",
+                    fontStyle: "italic",
+                    margin: "1.5rem 0 0.5rem 0",
+                  }}
+                >
+                  {status.text}
+                </p>
+              )}
               <br />
 
-              <button className={homeStyles.request_button} type="submit">
+              <button
+                className={homeStyles.request_button}
+                type="submit"
+                style={status.text ? { marginTop: 0 } : null}
+              >
                 Get Early Access
               </button>
             </form>
