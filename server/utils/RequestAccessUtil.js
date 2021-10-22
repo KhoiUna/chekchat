d5940770f0c656b7b8e1771a66b8c65c2e9e18d5
KhoiNguyen <thorwaitson@gmail.com>
2021-10-22 15:55:31 -0500

    update server RequestAccessUtil.js

--- server/utils/RequestAccessUtil.js
+++ server/utils/RequestAccessUtil.js
@@ -1,7 +1,18 @@
+const client = require("../db/client");
+
 module.exports = class RequestAccessUtil {
   static async saveAccessRequest(email) {
-    console.log(`save: ${email}`);
+    try {
+      const collection = client.db("chekchat").collection("early_access_users");
 
-    return true;
+      const response = await collection.insertOne({
+        email,
+        requested_date: new Date(new Date().toUTCString()),
+      });
+      return response;
+    } catch (err) {
+      console.error("Error saving early user ---util");
+      return null;
+    }
   }
 };
