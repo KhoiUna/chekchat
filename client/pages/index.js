dbacbdcbf8e8e4ecae0b3453f507026cc27c0b6b
KhoiNguyen <thorwaitson@gmail.com>
2021-10-27 00:08:35 -0500

    update youtube video

--- client/pages/index.js
+++ client/pages/index.js
@@ -147,7 +147,7 @@ export default function Home({}) {
           <iframe
             width="100%"
             height="500"
-            src="https://www.youtube.com/embed/rUhvXjrvzX8"
+            src="https://www.youtube-nocookie.com/embed/zOAgVhqno4U"
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
