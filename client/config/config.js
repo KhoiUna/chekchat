b513315fa9e119853390d8421e8aa03076e8fd0b
KhoiNguyen <thorwaitson@gmail.com>
2022-06-08 15:39:57 +0700

    client: update config.js

--- client/config/config.js
+++ client/config/config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  origin: process.env.NEXT_PUBLIC_ORIGIN || "https://api.chekchat.xyz",
-  // "https://chekapp.herokuapp.com",
+  origin: process.env.NEXT_PUBLIC_ORIGIN || "https://chekapp.herokuapp.com",
   launched: true,
   cookieOptions: {
     path: "/",
